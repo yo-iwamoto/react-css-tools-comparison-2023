51a877a457720d068e60d49a4e0ae5e3bedd13e6
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-03-12 17:22:13 +0900

    add apps/vanilla-extract

---
+++ apps/vanilla-extract/src/pages/index.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+export const title = style({
+  fontWeight: 'bold',
+  fontSize: '20px',
+  fontStyle: 'italic',
+});
---
+++ apps/vanilla-extract/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import * as styles from './index.css';
+
+export default function Page() {
+  return <h1 className={styles.title}>Hello World!</h1>;
+}
