be3ea9c90823802284f0faf866f89907f217b9bc
Yo Iwamoto <you.iwamoto918@gmail.com>
2023-03-12 17:19:42 +0900

    add apps/styled

---
+++ apps/styled/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  font-weight: bold;
+  font-size: 20px;
+  font-style: italic;
+`;
+
+export default function Page() {
+  return <Heading>Hello World!</Heading>;
+}
